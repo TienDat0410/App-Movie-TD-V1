754647bdbc172940f345b0c090958faed884082f
TienDat0410 <dat010401@gmail.com>
2023-05-30 16:28:57 +0700

    new link api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL:"http://localhost:8080",
-    // baseURL: "https://moviestdapi-production.up.railway.app",
+    // baseURL:"http://localhost:8080",
+    baseURL: "https://moviestdapi-production.up.railway.app",
 });
